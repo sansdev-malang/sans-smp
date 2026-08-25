0cfb5d7d3905e18b48e0a33aa448a703b61c30b2
Muhamad Ihwan <ihwan.2705@gmail.com>
2026-08-25 14:32:24 +0700

    fix: exempt download_token from cookie encryption to fix NProgress/toast dismiss timing

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -22,6 +22,10 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             'api/v1/hrd/*',
         ]);
+
+        $middleware->encryptCookies(except: [
+            'download_token',
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
         $exceptions->shouldRenderJsonWhen(
